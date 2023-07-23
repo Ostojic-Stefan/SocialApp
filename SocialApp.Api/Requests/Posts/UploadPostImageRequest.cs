748073f803c61c474a109ffa09c16eab0b9b9a68
name <ostojicstefan1337@gmail.com>
2023-07-23 13:53:39 +0200

    fix max file size upload

--- SocialApp.Api/Requests/Posts/UploadPostImageRequest.cs
+++ SocialApp.Api/Requests/Posts/UploadPostImageRequest.cs
@@ -4,7 +4,7 @@ namespace SocialApp.Api.Requests.Posts;
 
 public class UploadPostImageRequest
 {
-    [MaxFileSize(20)]
+    [MaxFileSize(20 * 1024 * 1024)]
     [AllowedExtensions(".jpg", ".jpeg", ".png")]
     public required IFormFile Img { get; set; }
 }
